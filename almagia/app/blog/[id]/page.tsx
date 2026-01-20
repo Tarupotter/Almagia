48779db058f7b9b3b4a42743e9b4d21fc1620e46
Taru Keskinen <taru.keskinen@lerniastudent.se>
2026-01-20 13:37:49 +0100

    Add back link navigation on blog post page

--- almagia/app/blog/[id]/page.tsx
+++ almagia/app/blog/[id]/page.tsx
@@ -1,4 +1,5 @@
 import BlogPost from "@/components/blog/BlogPost";
+import Link from "next/link";
 
 export default function BlogPostPage({
   params,
@@ -10,10 +11,19 @@ export default function BlogPostPage({
     "Här kommer du kunna skriva vad du än vill samt lägga foton om du vill.",
   imageUrl: "/flowersinhand.jpeg",
 };
+
   return (
-    <main className="py-20">
-        <BlogPost {...post} />
-        <p className="mt-6 text-center text-xs text-gray-500">ID: {params.id}</p>
+    <main className="mx-auto max-w-4xl px-6 py-16 space-y-6">
+      
+
+        <BlogPost {...post} /> 
+        <Link
+        href="/blog"
+        className="inline-block text-sm underline hover:opacity-80"
+      >
+        ← Tillbaka
+      </Link>
+        <p className="text-center text-xs text-gray-500">ID: {params.id}</p>
     </main>
   );
 }
