4cd5d0445c9191c5550c17847b6e76f1da5d0d40
Taru Keskinen <taru.keskinen@lerniastudent.se>
2026-01-28 14:48:32 +0100

    Change herosection to fade into background

--- almagia/components/home/HeroSection.tsx
+++ almagia/components/home/HeroSection.tsx
@@ -8,6 +8,7 @@ export default function HeroSection() {
           alt="Harmonisk bild"
           className="absolute w-full h-full object-cover object-[50%_70%]"
         />
+        <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-32 md:h-40 bg-gradient-to-b from-transparent to-[#F2EADF]" />
       </section>
     );
   }
